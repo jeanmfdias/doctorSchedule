af79cb3e9f8d65f84989c1c7ddd061fc9c3f1073
jeanmfdias <jeanmfdias@gmail.com>
2018-09-30 14:52:18 -0300

    translate labels to pt_BR

---
+++ resources/lang/pt_BR/format.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'date_time'         => 'd/m/Y às H:i',
+    'schedule_status'   => [
+        'CONSULTED' => 'success',
+        'CANCELED'  => 'danger',
+        'MISSED'    => 'warning',
+        'SCHEDULED' => 'info'
+    ],
+
+];
---
+++ resources/lang/pt_BR/views.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    'actions'       => 'Ação|Ações',
+    'active'        => 'Ativo',
+    'add'           => 'Adicionar',
+    'back_to'       => 'Voltar para',
+    'choose_file'   => 'Escolha um arquivo',
+    'delete'        => 'Excluir',
+    'doctors'       => 'Médico|Médicos',
+    'edit'          => 'Editar',
+    'errors'        => 'Erro|Erros',
+    'exams'         => 'Exame|Exames',
+    'has_exams'     => 'Têm exame?',
+    'inactive'      => 'Inativo',
+    'login'         => 'Login',
+    'logout'        => 'Logout',
+    'no'            => 'Não',
+    'patients'      => 'Paciente|Pacientes',
+    'register'      => 'Cadastrar',
+    'required'      => 'Obrigatório',
+    'reset'         => 'Limpar',
+    'schedules'     => 'Agendamento|Agendamentos',
+    'schedule_status'   => [
+        'CONSULTED' => 'Consultado',
+        'CANCELED'  => 'Cancelado',
+        'MISSED'    => 'Faltou',
+        'SCHEDULED' => 'Agendado'
+    ],
+    'save'          => 'Salvar',
+    'show'          => 'Ver',
+    'users'         => 'Usuário|Usuários',
+    'yes'           => 'Sim'
+
+];
